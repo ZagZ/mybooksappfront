c77af2d75fc662d92453f1debeb63ba83f9f5bda
javieramirez90 <javier.ramirez@aiesec.net>
2019-01-08 12:52:34 -0600

    Updated

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import "firebase/storage"
+
+<script src="https://www.gstatic.com/firebasejs/5.7.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDknt06f7sZwM-U_30Oan1W8ClRlg9JTno",
+    authDomain: "testing-4b1ff.firebaseapp.com",
+    databaseURL: "https://testing-4b1ff.firebaseio.com",
+    projectId: "testing-4b1ff",
+    storageBucket: "testing-4b1ff.appspot.com",
+    messagingSenderId: "380521457316"
+  };
+  firebase.initializeApp(config);
+</script>
